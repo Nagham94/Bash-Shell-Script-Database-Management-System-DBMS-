e009d894c9afa171d3e014988d4bbcbd7bf94761
heba <heba@localhost.localdomain>
2025-12-20 19:59:11 -0500

    Add listDatabases.sh

---
+++ Main_Menu/listDatabases.sh
@@ -0,0 +1,9 @@
+listDatabases(){
+        echo "Available Databases:"
+        if ls -d */ 1> /dev/null 2>&1; then
+                ls -d */ | sed 's#/##' # Replace / with nothing -> Remove / from the end of the directories name
+        else
+                echo "No databases are found"
+        fi
+}
+
