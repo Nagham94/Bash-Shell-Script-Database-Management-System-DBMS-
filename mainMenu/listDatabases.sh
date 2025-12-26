73f882fbb201b467120b4170b380219e8f0d8218
nagham94 <naghamfathy94@gmail.com>
2025-12-26 16:16:45 +0200

    update connectDatabase.sh dropDatabase.sh

--- mainMenu/listDatabases.sh
+++
@@ -1,9 +0,0 @@
-listDatabases(){
-        echo "Available Databases:"
-        if ls -d */ 1> /dev/null 2>&1; then
-                ls -d */ | sed 's#/##' # Replace / with nothing -> Remove / from the end of the directories name
-        else
-                echo "No databases are found"
-        fi
-}
-
