072e986125ffd487fa8e4ca3ce06c26a3e591952
nagham94 <naghamfathy94@gmail.com>
2025-12-26 17:03:11 +0200

    update DBMS.sh file

--- DBMS.sh
+++ DBMS.sh
@@ -10,12 +10,14 @@ read -p "Choose an option: " choice
 
 source "./Main_Menu/connectDatabase.sh"
 source "./Main_Menu/dropDatabase.sh"
+source "./Main_Menu/createDatabase.sh"
+source "./Main_Menu/listDatabase.sh" 
 
 case $choice in
 	1) create_database
-		;;
+		createDatabase ;;
 	2) list_database
-		;;
+		listDatabase ;;
 	3) connect_database
 		connectDatabase ;;
 	4) drop_database
